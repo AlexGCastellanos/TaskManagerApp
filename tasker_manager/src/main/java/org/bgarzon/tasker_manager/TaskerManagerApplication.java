292302c9ce26c1ed5e9544caee425765305162a6
Alexander Garzon <briangar94@hotmail.com>
2025-02-19 22:44:43 -0500

    First commit, add User entity

---
+++ tasker_manager/src/main/java/org/bgarzon/tasker_manager/TaskerManagerApplication.java
@@ -0,0 +1,13 @@
+package org.bgarzon.tasker_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskerManagerApplication.class, args);
+	}
+
+}
---
+++ tasker_manager/src/test/java/org/bgarzon/tasker_manager/TaskerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bgarzon.tasker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
